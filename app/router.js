056eb4e42ae2163784dbeb56bea6f8d0bd35ce8f
artegen <artegenus@gmail.com>
2017-12-05 11:15:28 +0200

    wip: Registration elements and data flow

--- app/router.js
+++ app/router.js
@@ -1,9 +1,49 @@
-() => (
-  <Router>
-    <Stack key="root">
-      <Scene key="login" component={Login} title="Login" />
-      <Scene key="register" component={Register} title="Register" />
-      <Scene key="home" component={Home} />
+import React from 'react';
+import {
+  Scene,
+  Router,
+  Actions,
+  Reducer,
+  ActionConst,
+  Overlay,
+  Tabs,
+  Modal,
+  Drawer,
+  Stack,
+  Lightbox,
+} from 'react-native-router-flux';
+
+const reducerCreate = params => {
+  const defaultReducer = new Reducer(params);
+  return (state, action) => {
+    console.log('ACTION:', action);
+    return defaultReducer(state, action);
+  };
+};
+
+const getSceneStyle = () => ({
+  backgroundColor: '#F5FCFF',
+  shadowOpacity: 1,
+  shadowRadius: 3,
+});
+
+// on Android, the URI prefix typically contains a host in addition to scheme
+// const prefix = Platform.OS === 'android' ? 'mychat://mychat/' : 'mychat://';
+
+export default () => (
+  <Router
+    createReducer={reducerCreate}
+    getSceneStyle={getSceneStyle}
+    // uriPrefix={prefix}
+  >
+    <Stack back backTitle="Back" key="register" duration={0}>
+      <Scene key="_register" component={Register} title="Register" />
+      <Scene
+        key="home"
+        component={Home}
+        title="Replace"
+        type={ActionConst.REPLACE}
+      />
     </Stack>
   </Router>
 );
